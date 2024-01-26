5798d4a454cc556939ef4d881c06742c2ea634f4
安和 <42889600+AHCorn@users.noreply.github.com>
2024-01-27 07:21:22 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,76 @@
-(function() {
-    var csvContent = "\uFEFFtitle,url\n"; 
-    var delay = 1000;  //等待时间，可自行修改以加快速度
+var delay = 2000; //等待时间
+var gen = listGen();
+var csvContent = "\uFEFF";
+csvContent += "folder,title,url\n";
 
-    function escapeCSV(field) {
-        return '"' + String(field).replace(/"/g, '""') + '"';
-    }
+function getCSVFileName() {
+    var userName = $("#h-name").text();
+    return userName + "的收藏夹.csv";
+}
 
-    function collectData() {
-        $(".fav-video-list > li > a.title").each(function() {
-            var title = $(this).text().replace(/,/g, '');
-            if (title !== "已失效视频") {
-                var url = 'https:' + $(this).attr("href");
-                csvContent += escapeCSV(title) + "," + escapeCSV(url) + "\n";
-            }
-        });
-    }
+function getFolderName() {
+    return $("#fav-createdList-container .fav-item.cur a.text").text().trim();
+}
 
-    function processPage() {
-        collectData();
+function escapeCSV(field) {
+    return '"' + String(field).replace(/"/g, '""') + '"';
+}
 
-        if ($(".be-pager-next").length > 0 && $(".be-pager-next").is(":visible")) {
-            $(".be-pager-next").click();
-            setTimeout(processPage, delay);
-        } else {
-            downloadCSV();
+function getVideosFromPage() {
+    var results = [];
+    var folderName = getFolderName().replace(/\//g, '\\'); // Replace / with \
+    $(".fav-video-list > li > a.title").each(function() {
+        var title = $(this).text().replace(/,/g, '');
+        if (title !== "已失效视频") {
+            var url = 'https:' + $(this).attr("href");
+            results.push(escapeCSV(folderName) + ',' + escapeCSV(title) + ',' + escapeCSV(url));
         }
+    });
+    return results.join('\n');
+}
+
+function processVideos() {
+    csvContent += getVideosFromPage() + '\n'; // Add a newline after each page of videos
+    if ($(".be-pager-next:visible").length == 0) {
+        setTimeout(changeList, delay);
+    } else {
+        $(".be-pager-next").click();
+        setTimeout(processVideos, delay);
     }
+}
 
-    function downloadCSV() {
-        var blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
-        var url = URL.createObjectURL(blob);
-        var dataWindow = window.open("", "_blank");
-
-        dataWindow.document.write('<a href="' + url + '" download="bilibili_favorites.csv">Download CSV</a>');
-        dataWindow.document.querySelector('a').click();
+function* listGen() {
+    for (var list of $("#fav-createdList-container .fav-item a").get()) {
+        yield list;
     }
+}
 
-    processPage();
-})();
+function changeList() {
+    var list = gen.next().value;
+    if (list) {
+        list.click();
+        setTimeout(processVideos, delay);
+    } else {
+        downloadCSV();
+    }
+}
+
+function downloadCSV() {
+    var fileName = getCSVFileName();
+    var blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    var url = URL.createObjectURL(blob);
+
+    var win = window.open();
+    if (win) {
+        win.document.open();
+        win.document.write('<html><body>');
+        win.document.write('<a href="' + url + '" download="' + fileName + '">点击下载</a>');
+        win.document.write('<script>document.querySelector("a").click();</script>');
+        win.document.write('</body></html>');
+        win.document.close();
+    } else {
+        alert('下载窗口被浏览器阻止，请先在设置里允许网页弹窗后重试。');
+    }
+}
+
+changeList();
